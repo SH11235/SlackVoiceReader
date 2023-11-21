c8e2f1dc9993106ef53eb3673ff0a764f93cbab8
hiraoka <aabceh1a1a2i3u5o8@gmail.com>
2023-11-22 02:24:21 +0900

    Add log dependency for logging messages

--- rust/script/src/main.rs
+++ rust/script/src/main.rs
@@ -3,45 +3,59 @@ use core::{
     play_audio_data, save_audio_data_to_file,
 };
 use dotenv::dotenv;
+use log::{error, info};
 use std::{env, thread, time::Duration};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
-    let slack_token = env::var("SLACK_TOKEN")?;
-    let channel_id = env::var("CHANNEL_ID")?;
-    let thread_ts = env::var("THREAD_TS")?;
-    let voicevox_url = env::var("VOICEVOX_URL")?;
+    let slack_token = env::var("SLACK_TOKEN").expect("SLACK_TOKEN is not set in .env file");
+    let channel_id = env::var("CHANNEL_ID").expect("CHANNEL_ID is not set in .env file");
+    let thread_ts = env::var("THREAD_TS").expect("THREAD_TS is not set in .env file");
+    let voicevox_url = env::var("VOICEVOX_URL").expect("VOICEVOX_URL is not set in .env file");
     let synthesize_endpoint = format!("{}/audio_query", voicevox_url);
     let audio_endpoint = format!("{}/synthesis", voicevox_url);
     let device = get_user_device()?;
-    let speaker_style_id = env::var("SPEAKER_STYLE_ID")?;
+    let speaker_style_id =
+        env::var("SPEAKER_STYLE_ID").expect("SPEAKER_STYLE_ID is not set in .env file");
 
-    let mut latest_timestamp = thread_ts;
+    let mut latest_timestamp = thread_ts.clone();
 
     loop {
-        let messages = fetch_slack_messages(&slack_token, &channel_id, &latest_timestamp).await?;
+        match fetch_slack_messages(&slack_token, &channel_id, &thread_ts).await {
+            Ok(messages) => {
+                if let Some((ts, text)) = get_new_message(&messages) {
+                    if ts != latest_timestamp {
+                        latest_timestamp = ts;
 
-        if let Some((ts, text)) = get_new_message(&messages) {
-            if ts != latest_timestamp {
-                latest_timestamp = ts;
+                        info!("New message: {}", text);
 
-                let audio_data = get_audio_data_from_voicevox(
-                    &text,
-                    &speaker_style_id,
-                    &synthesize_endpoint,
-                    &audio_endpoint,
-                )
-                .await?;
+                        match get_audio_data_from_voicevox(
+                            &text,
+                            &speaker_style_id,
+                            &synthesize_endpoint,
+                            &audio_endpoint,
+                        )
+                        .await
+                        {
+                            Ok(audio_data) => {
+                                // デバッグモードでのみ音声データをファイルに保存
+                                #[cfg(debug_assertions)]
+                                save_audio_data_to_file(&audio_data, "output.wav").await?;
 
-                // デバッグモードでのみ音声データをファイルに保存
-                #[cfg(debug_assertions)]
-                save_audio_data_to_file(&audio_data, "output.wav").await?;
-
-                play_audio_data(&device, &audio_data).await?;
+                                play_audio_data(&device, &audio_data).await?;
+                            }
+                            Err(e) => {
+                                error!("Failed to get audio data: {}", e);
+                            }
+                        }
+                    }
+                }
+            }
+            Err(e) => {
+                error!("Failed to fetch messages: {}", e);
             }
         }
-
         thread::sleep(Duration::from_secs(1));
     }
 }
