ecdcddb05c799cc786e1e453c1ff689a14339af1
hiraoka <aabceh1a1a2i3u5o8@gmail.com>
2023-11-23 22:23:49 +0900

    各種設定値入力フォーム作成

--- rust/desktop/src-tauri/src/main.rs
+++ rust/desktop/src-tauri/src/main.rs
@@ -1,15 +1,37 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+use tauri::command;
+use tauri::Error;
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Settings {
+    slack_token: String,
+    thread_url: String,
+    voicevox_url: String,
+    speaker_style_id: String,
+}
+
+#[command]
+fn load_settings() -> Result<Settings, Error> {
+    let file = File::open("settings.json")?;
+    let settings: Settings = serde_json::from_reader(file)?;
+    Ok(settings)
+}
+
+#[command]
+fn save_settings(settings: Settings) -> Result<(), Error> {
+    let file = File::create("settings.json")?;
+    serde_json::to_writer(file, &settings)?;
+    Ok(())
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![load_settings, save_settings])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
---
+++ rust/desktop/src/SettingsForm.tsx
@@ -0,0 +1,93 @@
+import { invoke } from "@tauri-apps/api/tauri";
+import { FormEvent, useEffect, useState } from "react";
+
+interface Settings {
+  slackToken: string;
+  threadUrl: string;
+  voicevoxUrl: string;
+  speakerStyleId: string;
+}
+
+function SettingsForm() {
+  const [settings, setSettings] = useState<Settings>({
+    slackToken: "",
+    threadUrl: "",
+    voicevoxUrl: "",
+    speakerStyleId: "",
+  });
+
+  useEffect(() => {
+    const loadSettings = async () => {
+      try {
+        const settings = await invoke<Settings>("load_settings");
+        console.log("Loaded settings:", settings);
+        setSettings({
+          slackToken: settings.slackToken || "",
+          threadUrl: settings.threadUrl || "",
+          voicevoxUrl: settings.voicevoxUrl || "",
+          speakerStyleId: settings.speakerStyleId || "",
+        });
+      } catch (err) {
+        console.error("Failed to load settings:", err);
+      }
+    };
+
+    loadSettings();
+  }, []);
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    invoke("save_settings", { settings });
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSettings({
+      ...settings,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Slack Token:
+        <input
+          type="text"
+          name="slackToken"
+          value={settings.slackToken}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Thread URL:
+        <input
+          type="text"
+          name="threadUrl"
+          value={settings.threadUrl}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        VoiceVox URL:
+        <input
+          type="text"
+          name="voicevoxUrl"
+          value={settings.voicevoxUrl}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Speaker Style ID:
+        <input
+          type="text"
+          name="speakerStyleId"
+          value={settings.speakerStyleId}
+          onChange={handleChange}
+        />
+      </label>
+      <input type="submit" value="Save" />
+    </form>
+  );
+}
+
+export default SettingsForm;
